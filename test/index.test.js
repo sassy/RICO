49d21c492c1b6d7933aba17912cb97bdefaac2ba
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-01-15 01:26:46 +0900

    add test.

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+var request = require('supertest');
+var app = require('../app');
+
+describe('index', function() {
+    it ('should return 200', function(done) {
+        request(app)
+            .get('/')
+            .expect(200)
+            .end(function(err, ret) {
+                if (err) {
+                    throw err;
+                }
+                done();
+            });
+        
+    });
+});
