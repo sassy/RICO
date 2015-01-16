3c82e3fa167bf7cd362c30d8c3aa2e5150998b4c
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-01-17 01:22:46 +0900

    save peer id for first connection. used redis.

--- sio.js
+++ sio.js
@@ -1,11 +1,27 @@
 
 
-module.exports = function(server) {
+module.exports = function(server) {    
     var io = require('socket.io').listen(server);
+    var redis = require("redis");
+    var redisClient = redis.createClient();
+    var self_id = null;
     io.sockets.on('connection', function(socket) {
+        redisClient.hkeys("users", function(err, replies) {
+            replies.forEach(function(reply, i) {
+                socket.emit("recieveid", reply);
+            });
+        });
         socket.on("sendid", function(id) {
             console.log(id);
+            self_id = id;
+            redisClient.hset("users",  self_id, "open", redis.print);
             socket.broadcast.emit("recieveid", id);
+        });
+
+        socket.on("disconnect", function() {
+            if (self_id != null) {
+                redisClient.hdel("users", self_id, redis.print);
+            }
         });
     });
 };
