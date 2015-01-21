fc6ab23396fadef37cca5375fd8874b41d69f42c
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-01-21 23:58:15 +0900

    add ENV value for skyway api key.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,69 +1,71 @@
-var peer = new Peer({key : ''});
+function rfc(api_key) {
+    var peer = new Peer({key : api_key});
 
-var peer_id;
-peer.on('open', function(id) {
-    var socket = io.connect();
-    console.log(io);
-    socket.on('connect', function() {
-        console.log(id);
-        socket.emit("sendid", id);
-    });
-    socket.on('recieveid', function(id) {
-        peer_id = id;
-        $("#peerlist").append('<a href="#" class="list-group-item" id="' + peer_id + '">' + peer_id + '</a>').bind('click', offer);
-    });
-    socket.on('removeid', function(id) {
-        $("#" + id).remove();
-    });
-});
+    var peer_id;
 
-function offer() {
-    if (navigator.webkitGetUserMedia) {
-        navigator.getUserMedia = navigator.webkitGetUserMedia;
-    } else if(navigator.mozGetUserMedia) {
-        navigator.getUserMedia = navigator.mozGetUserMedia;
-    }
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia({video:true, audio:true}, function(mediaStream) {
-            var call = peer.call(peer_id, mediaStream);
-            call.on('stream', function(stream) {
-                var video = document.querySelector('video');
-                video.src = window.URL.createObjectURL(stream);
-            });
-        }, function(e) {
-            console.log(e);
+    peer.on('open', function(id) {
+        var socket = io.connect();
+        socket.on('connect', function() {
+            socket.emit("sendid", id);
+            $("#peer_id").text(id);
         });
-    }
-    console.log(peer_id);
-}
-
-peer.on('connection', function(conn) {
-    conn.on('data', function(data) {
-        document.getElementById('message').innerText = data;
-    });
-});
-
-var answer_call = null;
-peer.on('call', function(call) {
-    $('#ans_button').removeAttr('disabled');
-    answer_call = call;
-});
-
-function answer() {
-    if (navigator.webkitGetUserMedia) {
-        navigator.getUserMedia = navigator.webkitGetUserMedia;
-    } else if(navigator.mozGetUserMedia) {
-        navigator.getUserMedia = navigator.mozGetUserMedia;
-    }
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia({video:true, audio:true}, function(mediaStream) {
-            answer_call.answer(mediaStream);
-            answer_call.on('stream', function(stream) {
-                var video = document.querySelector('video');
-                video.src = window.URL.createObjectURL(stream);
-            });
-        }, function(e) {
-            console.log(e);
+        socket.on('recieveid', function(id) {
+            peer_id = id;
+            $("#peerlist").append('<a href="#" class="list-group-item" id="' + peer_id + '">' + peer_id + '</a>').bind('click', offer);
         });
+        socket.on('removeid', function(id) {
+            $("#" + id).remove();
+        });
+    });
+
+    function offer() {
+        if (navigator.webkitGetUserMedia) {
+            navigator.getUserMedia = navigator.webkitGetUserMedia;
+        } else if(navigator.mozGetUserMedia) {
+            navigator.getUserMedia = navigator.mozGetUserMedia;
+        }
+        if (navigator.getUserMedia) {
+            navigator.getUserMedia({video:true, audio:true}, function(mediaStream) {
+                var call = peer.call(peer_id, mediaStream);
+                call.on('stream', function(stream) {
+                    var video = document.querySelector('video');
+                    video.src = window.URL.createObjectURL(stream);
+                });
+            }, function(e) {
+                console.log(e);
+            });
+        }
+        console.log(peer_id);
+    }
+
+    peer.on('connection', function(conn) {
+        conn.on('data', function(data) {
+            document.getElementById('message').innerText = data;
+        });
+    });
+
+    var answer_call = null;
+    peer.on('call', function(call) {
+        $('#ans_button').removeAttr('disabled');
+        answer_call = call;
+    });
+
+    function answer() {
+        if (navigator.webkitGetUserMedia) {
+            navigator.getUserMedia = navigator.webkitGetUserMedia;
+        } else if(navigator.mozGetUserMedia) {
+            navigator.getUserMedia = navigator.mozGetUserMedia;
+        }
+        if (navigator.getUserMedia) {
+            navigator.getUserMedia({video:true, audio:true}, function(mediaStream) {
+                answer_call.answer(mediaStream);
+                answer_call.on('stream', function(stream) {
+                    var video = document.querySelector('video');
+                    video.src = window.URL.createObjectURL(stream);
+                });
+            }, function(e) {
+                console.log(e);
+            });
+        }
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: "Rico: WebRTC conference service" });
+  res.render('index', { 
+      title: "Rico: WebRTC conference service",
+      api_key : process.env.SKYWAY_API_KEY,
+  });
 });
 
 module.exports = router;
