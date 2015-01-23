aa83fe474ea090bd58e3d4f75b92164d49852d91
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-01-23 23:12:36 +0900

    fix closing process.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,24 +1,41 @@
+var answer_call = null;
+
 function rfc(api_key) {
     var peer = new Peer({key : api_key});
 
     var peer_id;
 
     peer.on('open', function(id) {
+        peer_id = id;
         var socket = io.connect();
         socket.on('connect', function() {
             socket.emit("sendid", id);
             $("#peer_id").text(id);
         });
         socket.on('recieveid', function(id) {
-            peer_id = id;
-            $("#peerlist").append('<a href="#" class="list-group-item" id="' + peer_id + '">' + peer_id + '</a>').bind('click', offer);
+            $("#peerlist").append('<a href="#" class="list-group-item" id="' + id + '">' + id + '</a>').bind('click', offer);
         });
         socket.on('removeid', function(id) {
             $("#" + id).remove();
         });
+
+        peer.on('close', function(id) {
+            if (peer_id !== undefined) {
+                socket.emit('removeid', peer_id);
+                peer_id = undefined;
+            }
+        });
+
+        window.onbeforeunload = function() {
+            console.log("remove:" +  peer_id);
+            socket.emit('removeid', peer_id);
+            peer.destroy();
+            peer_id = undefined;
+        };
     });
 
-    function offer() {
+    function offer(event) {
+        var call_id = event.target.id;
         if (navigator.webkitGetUserMedia) {
             navigator.getUserMedia = navigator.webkitGetUserMedia;
         } else if(navigator.mozGetUserMedia) {
@@ -26,7 +43,7 @@ function rfc(api_key) {
         }
         if (navigator.getUserMedia) {
             navigator.getUserMedia({video:true, audio:true}, function(mediaStream) {
-                var call = peer.call(peer_id, mediaStream);
+                var call = peer.call(call_id, mediaStream);
                 call.on('stream', function(stream) {
                     var video = document.querySelector('video');
                     video.src = window.URL.createObjectURL(stream);
@@ -35,7 +52,7 @@ function rfc(api_key) {
                 console.log(e);
             });
         }
-        console.log(peer_id);
+        console.log(call_id);
     }
 
     peer.on('connection', function(conn) {
@@ -44,28 +61,27 @@ function rfc(api_key) {
         });
     });
 
-    var answer_call = null;
     peer.on('call', function(call) {
         $('#ans_button').removeAttr('disabled');
         answer_call = call;
     });
+}
 
-    function answer() {
-        if (navigator.webkitGetUserMedia) {
-            navigator.getUserMedia = navigator.webkitGetUserMedia;
-        } else if(navigator.mozGetUserMedia) {
-            navigator.getUserMedia = navigator.mozGetUserMedia;
-        }
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia({video:true, audio:true}, function(mediaStream) {
-                answer_call.answer(mediaStream);
-                answer_call.on('stream', function(stream) {
-                    var video = document.querySelector('video');
-                    video.src = window.URL.createObjectURL(stream);
-                });
-            }, function(e) {
-                console.log(e);
+function answer() {
+    if (navigator.webkitGetUserMedia) {
+        navigator.getUserMedia = navigator.webkitGetUserMedia;
+    } else if(navigator.mozGetUserMedia) {
+        navigator.getUserMedia = navigator.mozGetUserMedia;
+    }
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video:true, audio:true}, function(mediaStream) {
+            answer_call.answer(mediaStream);
+            answer_call.on('stream', function(stream) {
+                var video = document.querySelector('video');
+                video.src = window.URL.createObjectURL(stream);
             });
-        }
+        }, function(e) {
+            console.log(e);
+        });
     }
 }
