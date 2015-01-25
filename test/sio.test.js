a10e510a24ea06438dfeb298e1dc3efa6a794bb7
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-01-25 10:50:34 +0900

    fix sio test. forget to listen of server.

--- test/sio.test.js
+++ test/sio.test.js
@@ -13,13 +13,14 @@ var assert = require('power-assert');
 
 describe('sio', function() {
     it ('sio test', function(done) {
+        server.listen(3000);
         var redisClient = require("node-redis-mock").createClient();
         var spy = sinon.spy(redisClient, "hkeys");
         assert(spy.called === false);
         var sio = require('../sio')(server, redisClient);
         var socket = io.connect("http://localhost:3000", options);
         socket.on('connect', function() {
-            assert(spy.called === false);
+            assert(spy.called === true);
             done();
         });
     });
