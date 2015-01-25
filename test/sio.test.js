034612dba943409e4790c51ca8a1e7957708ccc0
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-01-26 01:46:24 +0900

    update sio test.

--- test/sio.test.js
+++ test/sio.test.js
@@ -2,7 +2,6 @@
 
 var app = require('../app');
 var http = require('http');
-var server = http.createServer(app);
 var sinon = require('sinon');
 var io = require('socket.io-client');
 var options = {
@@ -12,16 +11,33 @@ var options = {
 var assert = require('power-assert');
 
 describe('sio', function() {
-    it ('sio test', function(done) {
+    var server;
+    beforeEach(function() {
+        server = http.createServer(app);
         server.listen(3000);
+    });
+
+    afterEach(function() {
+        server.close();
+    });
+
+    it ('sio test', function() {
+        var redisClient = require("node-redis-mock").createClient();
+        var spy = sinon.spy(redisClient, "hkeys");
+        assert(spy.called === false);
+        var sio = require('../sio')(server, redisClient);
+        assert(spy.called === true);
+    });
+
+    it ('connection test', function(done) {
         var redisClient = require("node-redis-mock").createClient();
         var spy = sinon.spy(redisClient, "hkeys");
         assert(spy.called === false);
         var sio = require('../sio')(server, redisClient);
         var socket = io.connect("http://localhost:3000", options);
         socket.on('connect', function() {
-            assert(spy.called === true);
             done();
         });
     });
+
 });
